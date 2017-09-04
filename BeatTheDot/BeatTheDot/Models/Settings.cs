bfd786bea06b530ba4733e824e6383fb64895f81
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-09-04 15:35:25 -0300

    Fix settings

--- BeatTheDot/BeatTheDot/Models/Settings.cs
+++ BeatTheDot/BeatTheDot/Models/Settings.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
-
+using System.Xml.Serialization;
 using Xamarin.Forms;
 
 namespace BeatTheDot.Models
@@ -13,7 +14,37 @@ namespace BeatTheDot.Models
         public string Pass { get; set; }
         public int Tolerance { get; set; } = 10;
         public int LunchTime { get; set; } = 60;
-        public TimeSpan LunchAt { get; set; } = new TimeSpan(11, 30, 0);
-        public TimeSpan Worktime { get; set; } = new TimeSpan(8, 0, 0);
+
+        private TimeSpan lunchAt { get; set; } = new TimeSpan(11, 30, 0);
+
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never), XmlIgnore]
+        public TimeSpan LunchAt
+        {
+            get { return this.lunchAt; }
+            set { this.lunchAt = value; }
+        }
+
+        [XmlElement("LunchAt")]
+        public long LunchAtTicks
+        {
+            get { return this.lunchAt.Ticks; }
+            set { this.lunchAt = new TimeSpan(value); }
+        }
+
+        private TimeSpan worktime { get; set; } = new TimeSpan(8, 0, 0);
+
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never), XmlIgnore]
+        public TimeSpan Worktime
+        {
+            get { return this.worktime; }
+            set { this.worktime = value; }
+        }
+
+        [XmlElement("Worktime")]
+        public long WorktimeTicks
+        {
+            get { return this.worktime.Ticks; }
+            set { this.worktime = new TimeSpan(value); }
+        }
     }
 }
